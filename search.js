cc3f44292b14599177b341b99c8aa4e154030d00
Mathias Ryckewaert <mathryckdel@free.fr>
2021-03-22 21:28:05 +0100

    do the skeleton of the search.js

---
+++ search.js
@@ -0,0 +1,33 @@
+function openPage(){
+    const search = document.getElementById("inputSearch").value;
+    if( search === "cap"  || search === "Cap"  || search === "CAp" || search === "CAP"){
+        window.open("#.html","_self");
+    }
+    else if(search === "mug" || search === "Mug" || search === "MUg" || search === "MUG" || search === "cup" || search === "Cup" || search === "CUp" || search === "CUP"){
+        window.open("#.html","_self");
+    }
+    else if(search === "t-shirt" || search === "T-shirt" || search === "T-Shirt" || search === "tshirt" || search === "Tshirt"){
+        window.open("#.html","_self");
+    }
+    else if(search === "stylo" || search === "Stylo" || search === "pen" || search === "Pen" || search === "PEn" || search === "PEN"){
+        window.open("#.html","_self");
+    }
+    else if(search === "plant" || search === "Plant"){
+        window.open("#.html","_self");
+    }
+    else if(search === "apron" || search === "Apron"){
+        window.open("#.html","_self");
+    }
+    else if(search === "Basile" || search === "The pro player" || search === "The best quality" || search === "The best of the BEST"){
+        window.open("#.html","_self");
+    }
+    else if(search === "Clément" || search === "The beginner"){
+        window.open("#.html","_self");
+    }
+    else if(search === "sticker" || search === "Stickers" || search === "Sticker" || search === "stickers"){
+        window.open("#.html","_self");
+    }
+    else {
+        alert("Page not found, please search something else ;)")
+    }
+}
