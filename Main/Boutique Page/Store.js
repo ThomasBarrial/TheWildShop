0690bd256699a68a5f56c52067d9825fc9819b69
Thomas <barrialthomas@gmail.com>
2021-03-24 11:45:32 +0100

    newCommit

--- Main/Boutique Page/Store.js
+++ Main/Boutique Page/Store.js
@@ -1,3 +1,35 @@
+// MAIN NAV //
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+      //toogle nav
+      nav.classList.toggle('nav-active');
+      
+      //animate links
+      navLinks.forEach((link, index)=>{
+          if(link.style.animation) {
+              link.style.animation = ''
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+          }
+      });
+
+      //burger animation
+      burger.classList.toggle('toggle');
+  });
+ 
+}
+
+navSlide();
+
+
+
+
+// STORE NAV //
+
 const togglebutton = document.getElementById("toggle-button");
 const sidebar = document.getElementById("side-bar");
 
--- Main/HomePage/homeScript.js
+++ Main/HomePage/homeScript.js
@@ -1,23 +1,27 @@
-const togglebutton = document.getElementById("toggle-button");
-const sidebar = document.getElementById("burgerNav");
-const menu = document.getElementById("menu");
-
-
-togglebutton.addEventListener("click", function(){
-    sidebar.classList.toggle("active");
-    show();
-});
-
-function show() {        
-
-
-    if(menu.style.display != "none" ){
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-    };
-}
-
-if(screen.width < 900){
-window.onload = show
-}
+// MAIN NAV //
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+  
+    burger.addEventListener('click', () => {
+        //toogle nav
+        nav.classList.toggle('nav-active');
+        
+        //animate links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+        });
+  
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+   
+  }
+  
+  navSlide();
+  
